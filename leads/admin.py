b85df0ee446f677301d4f42fcc206f5dab040833
Sameer Tariq <engineersameertariq@gmail.com>
2026-09-12 00:17:36 +0500

    Implement lead phases and assignments

--- leads/admin.py
+++ leads/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Lead
+
+from .models import Lead, Phase, PhaseAssignment, PhaseEngineer
 
 
 @admin.register(Lead)
@@ -24,4 +25,62 @@ class LeadAdmin(admin.ModelAdmin):
         "project_name",
         "client_name",
         "client_email",
+    )
+
+
+@admin.register(Phase)
+class PhaseAdmin(admin.ModelAdmin):
+    list_display = (
+        "lead",
+        "sequence",
+        "type",
+        "status",
+        "start_date",
+        "due_date",
+        "completed_at",
+    )
+
+    list_filter = (
+        "type",
+        "status",
+    )
+
+    search_fields = (
+        "lead__project_name",
+    )
+
+
+@admin.register(PhaseAssignment)
+class PhaseAssignmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "phase",
+        "manager",
+        "status",
+        "assigned_at",
+        "responded_at",
+    )
+
+    list_filter = (
+        "status",
+    )
+
+    search_fields = (
+        "manager__username",
+        "phase__lead__project_name",
+    )
+
+
+@admin.register(PhaseEngineer)
+class PhaseEngineerAdmin(admin.ModelAdmin):
+    list_display = (
+        "phase",
+        "engineer",
+        "assigned_by",
+        "assigned_at",
+    )
+
+    search_fields = (
+        "engineer__username",
+        "assigned_by__username",
+        "phase__lead__project_name",
     )
